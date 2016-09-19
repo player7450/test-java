acebb23f313e01d685a51c62f5bcde64e703c56e
player7450 <cszliu@gmail.com>
2016-09-19 10:47:38 +0800

    ..

--- src/main/java/lz/test/App.java
+++ src/main/java/lz/test/App.java
@@ -1,15 +1,11 @@
 package lz.test;
 
-import com.google.common.collect.Lists;
 import lz.test.service.IHelloWorldService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * main entrance
@@ -26,15 +22,5 @@ public class App {
         String name = "siyu";
         String hiMsg = helloWorldService.sayHello(name);
         logger.info(hiMsg);
-
-        testNull();
-    }
-
-    public static void testNull() {
-        List<String> abc = null;
-//        abc = Lists.newArrayList("a", "b", "c");
-        for (String str : abc) {
-            System.out.println(str);
-        }
     }
 }
