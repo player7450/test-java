6cc437ff732d8587f9f19f50e4b6836ff3b862f6
lz109550 <lz109550@alibaba-inc.com>
2017-09-15 17:43:09 +0800

    test

--- src/main/java/lz/test/App.java
+++ src/main/java/lz/test/App.java
@@ -1,7 +1,18 @@
 package lz.test;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.*;
 
 /**
  * main entrance
@@ -13,4 +24,9 @@ public class App {
         String hiMsg = "hi, siyu!";
         logger.info(hiMsg);
     }
+
+    public static void foo() {
+        
+    }
+
 }
